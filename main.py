af4d3bd94bc04cfdc835f980c0bb77e055e76146
Machi <machidelpino1@gmail.com>
2026-06-29 21:25:19 -0300

    Fix startup message typo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     print("study-tracker-cli")
-    print("Use --help to see avalible commands.")
+    print("Use --help to see available commands.")
 
 if __name__ == "__main__":
     main()
